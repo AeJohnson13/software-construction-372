15e4a3350a4cdcfec39565c6812d2b8d1d2ea8f5
Alex <aej800@gmail.com>
2025-01-16 22:04:33 -0900

    added new repos

--- README.md
+++
@@ -1,1 +0,0 @@
-# software-construction-372
---
+++ webdevoplment/FirstJS.js
@@ -0,0 +1,6 @@
+	
+	function myFirstJsFunction(){
+		console.log("inside my first JS file")
+		document.getElementById("demo").innerHTML = "Hello JavaScript!"
+		window.alert("System stalled");
+	}

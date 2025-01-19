a5b587c2643ba1e839ef4f34eea3c23e4ddd6567
Aejohnson13 <aej800@gmail.com>
2025-01-18 16:41:58 -0900

    started work on quiz 2

---
+++ 2quizJS/HomePageScripts.js
@@ -0,0 +1,3 @@
+function rotate() {
+	document.getElementById("slide_show").src = "4_images/DJI_0127.jpg" 
+} 
--- webdevoplment/FirstJS.js
+++ webdevoplment/FirstJS.js
@@ -1,5 +1,6 @@
 	
-	function myFirstJsFunction(){
+	function myFirstJsFunction()
+	{
 		console.log("inside my first JS file")
 		document.getElementById("demo").innerHTML = "Hello JavaScript!"
 		window.alert("System stalled");

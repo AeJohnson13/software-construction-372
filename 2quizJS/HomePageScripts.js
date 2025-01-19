065c7dde1a211fb5ed0f0467f361eb495bff6567
Aejohnson13 <aej800@gmail.com>
2025-01-18 17:15:13 -0900

    working on slideshow

--- 2quizJS/HomePageScripts.js
+++ 2quizJS/HomePageScripts.js
@@ -1,3 +1,16 @@
+let index = 0 
+
 function rotate() {
-	document.getElementById("slide_show").src = "4_images/DJI_0127.jpg" 
+	const images = [
+		"4_images/DJI_0127.jpg",
+		"4_images/DJI_0154.jpg",
+		"4_images/IMG_2392.JPEG", 
+		"4_images/IMG_5327.JPEG"
+	];
+	const current_time = Date(milliseconds)
+	if (
+		document.getElementById("slide_show").src = images[index]
+		index += 1;
+		index %= 4;  
+
 } 

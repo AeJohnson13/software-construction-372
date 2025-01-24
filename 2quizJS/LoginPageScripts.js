7714e0912064748c6115269234a4b155f64264fe
Alex <aej800@gmail.com>
2025-01-24 10:51:06 -0900

    finished part 2

--- 2quizJS/LoginPageScripts.js
+++ 2quizJS/LoginPageScripts.js
@@ -20,7 +20,7 @@ function checkUserName()
 // Will return true if inputString contains only letters A-Z or a-z,
 // otherwise returns false
 function characterCheck(inputString){
-  const re = /[a-zA-Z]+/
+  const re = /^[a-zA-Z]+?|^ ?/
   let invalidCharCheck = re.exec(inputString)
   return (inputString == invalidCharCheck)
 } 

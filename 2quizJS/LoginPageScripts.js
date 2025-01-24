e3176c7cea73e0501405774a98c3b21793b53ff7
Alex <aej800@gmail.com>
2025-01-23 15:30:20 -0900

    test

--- 2quizJS/LoginPageScripts.js
+++ 2quizJS/LoginPageScripts.js
@@ -1,7 +1,19 @@
+// characterCheck( inputString ) 
+// 
+
+
+
+document.getElementById("UserNameInput").value;
+
+
+
+
+
+// Will return true if inputString contains only letters A-Z or a-z,
+// otherwise returns false
 function characterCheck(inputString){
   const re = /[a-zA-Z]+/
-  let invalidCharCheck = re.exec(inputString);
-  if (invalidCharCHeck == inputString) { 
-  // not broken?
-  } 
+  let invalidCharCheck = re.exec(inputString)
+  return (inputString == invalidCharCheck)
 } 
+
